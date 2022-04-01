782b4d2ab8ba348436cc645a3a0f2ad8686e6ace
Alexandre Halopé <alexandrehalope@yahoo.fr>
2022-04-01 14:06:46 +0200

    :heavy_check_mark: Test pass

--- src/test/java/com/eseo/rest_server/service/VilleServiceTest.java
+++ src/test/java/com/eseo/rest_server/service/VilleServiceTest.java
@@ -4,16 +4,13 @@ import com.eseo.rest_server.entity.Ville;
 import com.eseo.rest_server.repository.VilleRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class VilleServiceTest {
 
